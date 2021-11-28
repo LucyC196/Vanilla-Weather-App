7e777a9f3a80b66d7d63a74c742693dc7f2fa46b
LucyC196 <94336377+LucyC196@users.noreply.github.com>
2021-11-28 16:35:16 +1000

    Improving id labels and creating functions to fetch live data from Open Weather API

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "74f3a3ae16656944facca16fcb9edb90";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rockhampton&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
