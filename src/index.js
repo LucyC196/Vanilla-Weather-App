d9c026df0102bf916aa3b5be933782734da4e790
LucyC196 <94336377+LucyC196@users.noreply.github.com>
2021-11-28 16:49:39 +1000

    Fetching further API data for uv index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let uvElement = document.querySelector("#uv");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.main.wind.speed);
 }
 
 let apiKey = "74f3a3ae16656944facca16fcb9edb90";
